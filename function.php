3f0febc142b4fed14307f382861be117f244b434
Peter Chen <peterchen2017125@gmail.com>
2023-02-10 12:04:55 +0800

    20230210

--- function.php
+++ function.php
@@ -1,1 +1,18 @@
-function
+<?php
+require_once("pdo.php");
+
+function RoleCheck()
+{
+    if (!isset($_SESSION["AUTH"])) {
+        header("Location: index.php");
+    }
+}
+
+function QueryCode()
+{
+    global $pdo;
+    $sql = "SELECT * FROM todo_list JOIN status ON todo_list.status = status.code JOIN usetodo ON usetodo.codes";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $result = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
